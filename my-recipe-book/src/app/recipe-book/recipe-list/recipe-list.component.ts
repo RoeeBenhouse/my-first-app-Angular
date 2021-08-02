094b7a64c51d4bc1020bd7d50797b644016852ee
RoeeBenhous <roibenhous2008@gmail.com>
2021-08-02 12:20:18 +0300

    Section 6: Recipe Book continue

--- my-recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ my-recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
@@ -7,9 +7,13 @@ import {Recipe} from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeDetailsEv = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('Test recipe',
       'This is an description for the test recipe',
+      'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg'),
+    new Recipe('Test2 recipe',
+      'This is an description for the test2 recipe',
       'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg'),
   ];
 
@@ -18,4 +22,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeClicked(recipeToShowDetails: Recipe) {
+    this.recipeDetailsEv.emit(recipeToShowDetails);
+  }
 }
